ec0a3bda677fb5cfad10e587dd59509e5f634922
ExFighter1607 <86557934+ExFighter1607@users.noreply.github.com>
2021-07-02 12:51:50 +0800

    Update syscompile.h

--- Storm-VWT/include/syscompile.h
+++ Storm-VWT/include/syscompile.h
@@ -33,7 +33,7 @@ extern "C"{
 	#define pause() system("pause")
 
 #elif  __linux__
-	#define _int_64_u unsigned      long
+	#define _int_64_u      unsigned long
     #define _int_64_t               long
 	_int_64_u randlu(void){
 		_int_64_u randomsd[2]={0};
@@ -55,7 +55,7 @@ extern "C"{
 	int pause(void){
 		printf("请按任意键继续...\n");
 		struct termios tm, tm_old;
-		int fd = STDIN_FILENO,c;
+		int fd = STDIN_FILENO,  c;
 		if (tcgetattr(fd, &tm) < 0){
 			return -1;
 		}
